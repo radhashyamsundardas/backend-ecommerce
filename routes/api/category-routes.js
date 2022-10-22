f268ad2d46fc3c39226e55f998dc1417a56df64d
hiren patel <hirenpatel0526@yahoo.com>
2022-10-22 12:58:52 -0700

    removed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { regexp } = require('sequelize/types/lib/operators');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
