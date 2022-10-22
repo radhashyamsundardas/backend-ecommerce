939d584bcf7ea3defb55a84d783409626bfbe876
hiren patel <hirenpatel0526@yahoo.com>
2022-10-21 17:12:46 -0700

    finished product js

--- models/Product.js
+++ models/Product.js
@@ -11,19 +11,41 @@ Product.init(
   {
     // define columns
     id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
 
     },
     product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
 
     },
+
     price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isNumeric :true,
+      },
 
     },
     stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue:10,
+      validate:{
+        isNumeric: true
+      },
 
     },
     category_id:{
-
+      type: DataTypes.INTEGER,
+      references: {
+        model:"category",
+        key: "id",
+      },
     },
   },
   {
