b9b66281145147f616c2723a3b7de516e645944e
hiren patel <hirenpatel0526@yahoo.com>
2022-10-22 13:17:38 -0700

    another get

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,8 +9,7 @@ router.get('/', (req, res) => {
  Category.findAll(
   {
     include: {
-      model:Product,
-      attributes: ['product_name']
+      model:Product, attributes: ['product_name']
     }
   }
  )
@@ -24,6 +23,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {id:req.params.id},
+    include:{ model:Product, attributes:["Category-id"]}
+  })
+  .then(information => res.json(information))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
