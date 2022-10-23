9ef855c72c01689db1941708c83894a6e30c65af
hiren patel <hirenpatel0526@yahoo.com>
2022-10-23 14:19:46 -0700

    product get

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  await Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [{
+      model: Tag,
+      attributes: ['id', 'tag_name'],
+      through: 'prduct_tag',
+    },
+    {
+      model: Category,
+      attributes: ['id', 'category_name'],
+    },
+  ],
+})
+.then ((productInfo) => {
+  res.json(productInfo);
+
+})
+.catch((err) => {
+  res.json(err);
 });
 
 // get one product
